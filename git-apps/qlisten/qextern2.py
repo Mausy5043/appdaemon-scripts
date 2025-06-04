14532fccc40058b4df5591df9b1f3bf081367c0e
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-06-04 20:16:26 +0200

    update2

--- git-apps/qlisten/qextern2.py
+++ git-apps/qlisten/qextern2.py
@@ -2,5 +2,10 @@ import appdaemon.plugins.hass.hassapi as hass  # type: ignore[import-untyped]
 
 
 class QExtern2(hass.Hass):  # type: ignore[misc]
-    def initialize(self):
-        pass
+  def initialize(self):
+      pass
+
+  def notif(self, message: str):
+      self.log(f"QExtern2 notif called with message: {message}")
+      # Here you can implement the logic to send the notification
+      # For example, using a service call or another method
--- git-apps/qlisten/qlisten2.py
+++ git-apps/qlisten/qlisten2.py
@@ -9,8 +9,8 @@ class QListen2(hass.Hass):  # type: ignore[misc]
         self.listen_state(self.state_changed, entity, attribute=attribute)
         # self.listen_state(xt_cb.state_changed, entity, attribute=attribute)
         self.log(f"\n\t*** Listening to entity: {entity}, attribute: {attribute}")
-        self.notifier = self.get_app("qextern")
+        self.notif = self.get_app("qextern")
 
     def state_changed(self, *args, **kwargs):
         self.log("qlisten self state changed in class ...")
-        self.notifier.notify("ping!")
+        self.notif.notif("ping!")
