b1b5d3b6d748790e8be32a3952961e82c4ed9686
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-06-04 20:14:47 +0200

    update

--- git-apps/qlisten/qextern2.py
+++ git-apps/qlisten/qextern2.py
@@ -4,18 +4,3 @@ import appdaemon.plugins.hass.hassapi as hass  # type: ignore[import-untyped]
 class QExtern2(hass.Hass):  # type: ignore[misc]
     def initialize(self):
         pass
-
-    def notify(self, message):
-        self.log(f"qextern notified : {message}")
-
-
-# def state_changed(fles, *args, **kwargs):
-#     # {entity} ({attribute}): {old} -> {new}
-#     try:
-#         fles.log(f"fles state changed in external ...")
-#     except:
-#         pass
-#     try:
-#         hass.log(f"hass state changed in external ...")
-#     except:
-#         pass
