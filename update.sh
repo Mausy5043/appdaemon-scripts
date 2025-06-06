a50a7ff5fb7790802b73a391a08dd38ec63416e4
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-06-06 11:04:11 +0200

    add script to copy apps

---
+++ update.sh
@@ -0,0 +1,47 @@
+#!/bin/env bash
+# sync the repository with git and update the local files
+
+branch_name="master"
+
+repo_dir="/addon_configs/git/appdaemon-scripts"
+if [ ! -d "${repo_dir}" ]; then
+    echo "Repository directory does not exist: ${repo_dir}"
+    exit 1
+fi
+# Ensure the repository directory is a git repository
+if [ ! -d "${repo_dir}/.git" ]; then
+    echo "Directory is not a git repository: ${repo_dir}"
+    exit 1
+fi
+
+apps_dir="/addon_configs/a0d7b954_appdaemon/apps"
+if [ ! -d "${apps_dir}" ]; then
+    echo "Apps directory does not exist: ${apps_dir}"
+    exit 1
+fi
+
+repo_apps_dir="${repo_dir}/git-apps"
+if [ ! -d "${repo_apps_dir}" ]; then
+    echo "Repository apps directory does not exist: ${repo_apps_dir}"
+    exit 1
+fi
+
+
+{
+    cd "${repo_dir}" || exit 1
+    # update the repository
+    git fetch origin || sleep 6
+    git fetch origin
+    git pull
+    git fetch origin
+    git checkout "${branch_name}"
+    # reset if local changes are present
+    git reset --hard "origin/${branch_name}" && git clean -f -d
+    # copy apps to the apps directory
+    echo "Copying apps from ${repo_apps_dir} to ${apps_dir}"
+    cp -rv "${repo_apps_dir}"/* "${apps_dir}/" || {
+        echo "Failed to copy apps from ${repo_apps_dir} to ${apps_dir}"
+        exit 1
+    }
+    echo "Apps updated successfully."
+}
