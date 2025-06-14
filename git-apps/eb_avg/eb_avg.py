31a7772570bee3bada3d8e51bf41c3f1075294e9
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-06-14 18:54:18 +0200

    INITIAL COMMIT

---
+++ git-apps/eb_avg/eb_avg.py
@@ -0,0 +1,26 @@
+import contextlib
+from collections import deque
+
+import appdaemon.plugins.hass.hassapi as hass  # type: ignore[import-untyped]
+
+"""Calculate moving average of Eigen Bedrijf to dampen peaks."""
+
+
+class EB_Avg(hass.Hass):  # type: ignore[misc]
+    def initialize(self):
+        self.sensor = "sensor.eigen_bedrijf"
+        self.avg_sensor = "sensor.eigen_bedrijf_avg"
+        self.values = deque(maxlen=6)  # Stores up to 60 values
+        self.listen_state(self.collect_value, self.sensor)
+        self.run_every(self.calculate_average, "now", 60)
+
+    def collect_value(self, entity, attribute, old, new, **kwargs):
+        with contextlib.suppress(ValueError):
+            self.values.append(float(new))
+
+    def calculate_average(self, **kwargs):
+        if self.values:
+            avg_value = round(sum(self.values) / len(self.values), 1)
+            self.set_state(self.avg_sensor, state=avg_value)
+        else:
+            self.set_state(self.avg_sensor, state=0)
