b6ded923237fa894ec5cff80d0b3ad250e6777f7
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-06-22 09:11:23 +0200

    secretize app

--- git-apps/scrts/scrts.py
+++ git-apps/scrts/scrts.py
@@ -8,17 +8,14 @@ class Secrets(hass.Hass):  # type: ignore[misc]
     def initialize(self):
         """Initialize the app."""
         self.log(f"============================== Secret dispenser v{VERSION} ====")
-        _s = self.get_tibber_token()
-        self.log(f"Retrieved secret: {_s}")
 
     def get_tibber_token(self) -> str:
         """Get the Tibber token from the secrets.yaml."""
         _scrt: str = ""
         if "tibber_token" not in self.args:
-            self.log("No 'tibber_token' found in args", level="ERROR")
-            return "NO TOKEN FOUND"
+            self.log("*** No 'tibber_token' found in args", level="ERROR")
+            return "NO_TOKEN_FOUND"
         _scrt = self.args["tibber_token"]
         if not _scrt:
-            self.log("Empty 'tibber_token' found in args", level="ERROR")
-            return ""
+            self.log("**** Empty 'tibber_token' found in args", level="ERROR")
         return _scrt
