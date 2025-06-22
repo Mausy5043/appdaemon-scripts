191b38a8f019632b5f1ff96d1cf11edd9a6ec33e
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-06-22 09:08:57 +0200

    update log

--- git-apps/eb_avg/eb_avg.py
+++ git-apps/eb_avg/eb_avg.py
@@ -11,7 +11,7 @@ VERSION = "1.0.0"
 
 class EigenBedrijf_Avg(hass.Hass):  # type: ignore[misc]
     def initialize(self):
-        self.log(f"===================================== EigenBedrijf_Avg v{VERSION} ====")
+        self.log(f"============================= EigenBedrijf_Avg v{VERSION} ====")
         self.sensor = "sensor.eigen_bedrijf"
         self.avg_sensor = "sensor.eigen_bedrijf_avg"
         self.values: deque[float] = deque(maxlen=6)
