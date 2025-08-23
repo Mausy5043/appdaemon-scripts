dbe8dc74ea385f8f4b9663590daf43ba761739b0
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-08-23 08:46:30 +0200

    double queue length

--- git-apps/eb_avg/eb_avg.py
+++ git-apps/eb_avg/eb_avg.py
@@ -6,15 +6,17 @@ import appdaemon.plugins.hass.hassapi as hass  # type: ignore[import-untyped]
 
 """Calculate moving average of Eigen Bedrijf to dampen peaks."""
 
-VERSION = "1.1.1"
-
+VERSION: str = "1.1.1"
+QLEN: int = 12
 
 class EigenBedrijf_Avg(hass.Hass):  # type: ignore[misc]
     def initialize(self):
         self.log(f"============================= EigenBedrijf_Avg v{VERSION} ====")
         self.sensor = "sensor.eigen_bedrijf"
         self.avg_sensor = "sensor.eigen_bedrijf_avg"
-        self.values: deque[float] = deque(maxlen=6)
+        self.values: deque[float] = deque(maxlen=QLEN)
+
+        # intialise callbacks
         self.listen_state(self.collect_value, self.sensor)
         self.run_every(self.calculate_average, "now", 60)
 
