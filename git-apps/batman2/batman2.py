68720ba86dfdc2a23c7edeb192da857b14750750
Maurice (mausy5043) Hendrix <mausy5043@users.noreply.github.com>
2025-10-03 21:21:02 +0200

    adjust spacing

--- git-apps/batman2/batman2.py
+++ git-apps/batman2/batman2.py
@@ -294,7 +294,7 @@ class BatMan2(hass.Hass):  # type: ignore[misc]
             self.log(
                 f"Today's pricelist           =  {
                     [f'{n:.3f}' for n in self.price['today']]
-                }\n                : cheap slots                 = [{
+                }\n               : cheap slots                 = [{
                     ', '.join(f'{v / 4:.2f}' for v in self.price['cheap_slot'])
                 }]\n               : expensive slots             = [{
                     ', '.join(f'{v / 4:.2f}' for v in self.price['expen_slot'])
